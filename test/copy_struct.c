aebb5f8bfd81b053584b84e271b9dce41ab63866
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2016-03-03 20:45:55 +0900

    Add a testcase which copies structs

---
+++ test/copy_struct.c
@@ -0,0 +1,17 @@
+#include "../libf.h"
+
+typedef struct {
+  int x;
+  int y;
+  int z;
+} S;
+
+int main() {
+  S s1 = { 42, 44, 46 };
+  S s2;
+  s2 = s1;
+  printf("%d %d %d\n", s2.x, s2.y, s2.z);
+  // TODO: There's an upstream bug in 8cc.
+  //S s3 = s1;
+  //printf("%d %d %d\n", s3.x, s3.y, s3.z);
+}

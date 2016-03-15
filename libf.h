974aeb3abfd2cce57e0d57f96893b9ac817f1b67
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2016-03-15 10:35:47 +0900

    Optimize my_div

--- libf.h
+++ libf.h
@@ -88,14 +88,28 @@ typedef struct {
 // Our 8cc doesn't support returning a structure value.
 // TODO: update 8cc.
 void my_div(unsigned int a, unsigned int b, div_t* o) {
-  unsigned int q = 0;
-  unsigned int n;
-  while (1) {
-    n = a - b;
-    if (n > a)
+  unsigned int d[24];
+  unsigned int r[24];
+  unsigned int i;
+  r[0] = 1;
+  for (i = 0;; i++) {
+    d[i] = b;
+    unsigned int nb = b + b;
+    if (nb > a || nb < b)
       break;
-    ++q;
-    a = n;
+    r[i+1] = r[i] + r[i];
+    b = nb;
+  }
+
+  unsigned int q = 0;
+  for (;; i--) {
+    unsigned int v = d[i];
+    if (a >= v) {
+      q += r[i];
+      a -= v;
+    }
+    if (i == 0)
+      break;
   }
   o->quot = q;
   o->rem = a;

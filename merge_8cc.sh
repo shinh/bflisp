c3f1a02cb8df298c0fc99ecdd9926e8ff7d0cc38
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2016-03-16 21:29:26 +0900

    Don't use 8cc to merge 8cc's code

--- merge_8cc.sh
+++ merge_8cc.sh
@@ -2,16 +2,11 @@
 
 set -e
 
-mkdir -p tmp8cc
-
 cat libf.h
 
-for c in 8cc/*.h 8cc/*.c; do
-    grep -v '#include <' $c > tmp$c
-done
+srcs="error.h list.h dict.h 8cc.h cpp.c debug.c dict.c  error.c gen.c lex.c list.c main.c parse.c string.c"
 
-for c in cpp.c debug.c dict.c  error.c gen.c lex.c list.c main.c parse.c string.c; do
-    echo "// $c start"
-    8cc/8cc -E tmp8cc/$c
-    echo "// $c end"
+for c in $srcs; do
+    ruby -p0e '$_.sub!(%q(#include "keyword.h"), File.read("8cc/keyword.h"))' \
+        < 8cc/$c | grep -v '#include'
 done
